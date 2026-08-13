e0923ad383888d26c68b4ee0958d2a4ae54d00ce
fredrik.kallin@gmail.com <fredrik.kallin@gmail.com>
2026-08-13 10:26:50 +0200

    push 2

---
+++ script.js
@@ -0,0 +1,18 @@
+const heading = document.getElementById("heading");
+const firstDescription = document.querySelector(".description");
+const buttons = document.querySelectorAll(".btn");
+const firstButton = document.querySelector(".btn");
+
+const button = document.querySelectorAll(".themeButton");
+const cardOne = document.querySelector(".cardOne");
+const cardTwo = document.querySelector(".cardTwo");
+
+button[0].addEventListener("click", () => {
+    cardOne.classList.toggle("dark");
+    cardTwo.classList.toggle("red");
+});
+
+button[1].addEventListener("click", () =>{
+     cardOne.classList.toggle("dark");
+    cardTwo.classList.toggle("red");   
+});
